843ff90acb9deefaece2c932f8154400a94779ce
nullpitch-dev <59587405+nullpitch-dev@users.noreply.github.com>
2020-07-12 23:53:11 +0900

    Update codes.py

--- codes.py
+++ codes.py
@@ -6,7 +6,6 @@ import math
 url_pur = 'https://raw.githubusercontent.com/nullpitch-dev/hj_public/master/ds_purchase_log.csv'
 url_cus = 'https://raw.githubusercontent.com/nullpitch-dev/hj_public/master/ds_customer_mst.csv'
 url_pro = 'https://raw.githubusercontent.com/nullpitch-dev/hj_public/master/ds_product_mst.csv'
-
 data_pur = pd.read_csv(url_pur)
 data_cus = pd.read_csv(url_cus)
 data_pro = pd.read_csv(url_pro)
@@ -15,15 +14,26 @@ data_pro = pd.read_csv(url_pro)
 url_antibio = "https://raw.githubusercontent.com/nullpitch-dev/hj_public/master/Antibiotic_70K_patinets.csv"
 data_antibio = pd.read_csv(url_antibio)
 
+# Ex04 Data ###########################################################################################
+url_corolla_1 = 'https://raw.githubusercontent.com/nullpitch-dev/hj_public/master/corolla_1.csv'
+url_corolla_2 = 'https://raw.githubusercontent.com/nullpitch-dev/hj_public/master/corolla_2.csv'
+data_corolla1 = pd.read_csv(url_corolla_1)
+data_corolla2 = pd.read_csv(url_corolla_2)
+
 
 #####################################################################################################
 #####################################################################################################
 #####################################################################################################
+
+
+# dummy variables ##############################
+df_dummy = pd.get_dummies(df_original, columns=df_original.columns[_from_:_to_], drop_first=True)
 
 
 # elif in lambda ##############################
 df_elif = df_elif.assign(new_col=df_elif["Col_A"].apply(lambda x: "value_1" if x <= condi_1 else (
                                                                   "value_2" if x >= condi_2 else "middle")))
+
 
 # enumerate and list comprehension ##############################
 # 결과 중 특정 조건을 만족하는 변수명 찾기
@@ -49,7 +59,7 @@ df_rank = df_rank.assign(rank=df_rank.rank(ascending=False, method="min"))
 #####################################################################################################
 
 
-# ANOVA #######################################
+# ANOVA #####################################################################################################
 # ANOVA Test 개념
 # 분산을 고려했을 때 표본들의 평균이 같다고 할 수 있는지 검증
 # H0 : 표본집단들의 모집단 평균이 같다
@@ -67,8 +77,10 @@ from statsmodels.stats.anova import anova_lm
 result = ols(formula="dependent)var ~ C(independent_var1) + independent_var2", data=df_anova).fit()
 anova_table = anova_lm(result)
 print(anova_table)
+###########################################################################################################
 
-# MultiComparison ##############################
+
+# MultiComparison ########################################################################################
 # 사후검정 #######################################
 from statsmodels.stats.multicomp import MultiComparison
 
@@ -87,9 +99,10 @@ group1 group2  meandiff  p-adj    lower      upper    reject
 # meandiff = group2_mean - group1_mean
 # reject = True : mean is different
 """
+###########################################################################################################
 
 
-# Association Rule ##############################
+# Association Rule #####################################################################################
 # [Support, Confidence, Lift 개념]
 #   지지도
 #     – Support = P(X ∩ Y)
@@ -110,13 +123,29 @@ asso_rules = association_rules(freq_items, metric="confidence", min_threshold=0.
 
 asso_rules = asso_rules.assign(check=asso_rules["antecedents"].apply(lambda x: "XXX" in x)) # XXX가 포함된 항목 check
 asso_rules = asso_rules[asso_rules["check"]].sort_values(by="lift", ascending=False)
+###########################################################################################################
 
 
-# Correlation ##############################
+# Correlation ##########################################################################################
 df_corr_result = df_corr[['var1', 'var2']].corr(method='pearson')
+###########################################################################################################
 
 
-# Logistic Regression ##############################
+# Liner Regression ##########################################################################################
+from statsmodels.api import add_constant, OLS
+
+train_X = add_constant(train_X)
+model = OLS(train_y, train_X)
+ols_result = model.fit()
+
+ols_result.pvalues # p-value가 필요하면
+ols_result.tvalues # t-value가 필요하면
+ols_result.rsquared # R2가 필요하면
+ols_result.rsquared_adj # Adjusted R2가 필요하면
+###########################################################################################################
+
+
+# Logistic Regression ##########################################################################
 from sklearn.linear_model import LogisticRegression
 
 train_X = df_lr['dep_A', 'dep_B', 'dep_C', 'dep_D', 'dep_E']
@@ -124,9 +153,10 @@ train_y = df_lr['indep']
 
 lr = LogisticRegression(C=100000, random_state=1234, penalty='l2', solver='newton-cg')
 model = lr.fit(train_X, train_y)
+###########################################################################################################
 
 
-# T-Test ##############################
+# T-Test ################################################################################################
 # H0 : 두 집단의 평균이 같다 (다르다고 할 수 없다)
 # [COMPARISON T-TEST vs. ANOVA]
 #     T-test is a hypothesis test that is used to compare the means of two populations.
@@ -137,3 +167,4 @@ model = lr.fit(train_X, train_y)
 from scipy.stats import ttest_ind
 
 p_val, t_val = ttest_ind(cd_y['high_p'], cd_n['high_p'])
+###########################################################################################################
