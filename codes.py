ede5414e8b1c741dcd5a51d4f184635fa243f1f1
nullpitch-dev <59587405+nullpitch-dev@users.noreply.github.com>
2020-08-16 15:21:56 +0900

    Update codes.py

--- codes.py
+++ codes.py
@@ -63,7 +63,7 @@ df_merge = pd.merge(data_left, data_right[["Col_A", "Col_B"]], how="left",
 df_merge = pd.merge(data_left, data_right[['Col_A']], how='left',
                      left_index=True, right_index=True)
 # 복수개의 column을 기준으로 merge 할 때:
-df_merge = pd.merge(data_left, data_right, how='inner', on=['Col_A', 'Col_B'])f
+df_merge = pd.merge(data_left, data_right, how='inner', on=['Col_A', 'Col_B'])
 
 
 ### Pivot_table for Association_rules ##########################################
